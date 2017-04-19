a6f917a4e5b3d5180a805c4f0c47d9900c1f7f84
promethwill88 <wfong.bk@gmail.com>
2017-04-18 21:25:41 -0700

    css work + materialize autocomplete

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,27 @@
 // CLIENT-SIDE JS
 console.log('app.js loaded!');
+
+
+
+
 var $restroomList;
 
 
 $(document).ready(function() {
+  $('input.autocomplete').autocomplete({
+    data: {
+      'Financial District': null,
+      'Japantown': null,
+      'Nob Hill': null
+    },
+    limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+    onAutocomplete: function(val) {
+      // Callback function when value is autcompleted.
+    },
+    minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+  });
+
+
   $restroomList = $('.list');
   $.ajax({
   	method: 'GET',
