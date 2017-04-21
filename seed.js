95a8ad5320d7b2ed89b53fe48cdf5787db882111
promethwill88 <wfong.bk@gmail.com>
2017-04-20 21:35:12 -0700

    added additional seed data to test search

--- seed.js
+++ seed.js
@@ -3,35 +3,119 @@
 
 var db = require("./models");
 
-var restroom_list = [ 
+var restroom_list = [
   {
-    location: "225 Bush St",
+    location: "1696 Hayes St.",
+    locationName: "Central Coffee Tea & Spices",  
+    type: "Cafe",
+    cleanliness: 4,
+    neighborhood: "Panhandle"
+  }, 
+  {
+    location: "2175 Chestnut St.",
+    locationName: "Delarosa",  
+    type: "Restaurant",
+    cleanliness: 5,
+    neighborhood: "Marina"
+  },
+  {
+    location: "3913 24th St.",
+    locationName: "Savor",  
+    type: "Restaurant",
+    cleanliness: 5,
+    neighborhood: "Noe Valley"
+  },
+  {
+    location: "429 Castro St.",
+    locationName: "Castro Theatre",  
+    type: "Movie Theater",
+    cleanliness: 4,
+    neighborhood: "Castro District"
+  },
+  {
+    location: "3349 20th St.",
+    locationName: "Shotwell's",  
+    type: "Bar",
+    cleanliness: 3,
+    neighborhood: "Mission District"
+  },
+  {
+    location: "2095 Union St.",
+    locationName: "Soul Cycle",  
+    type: "Gym",
+    cleanliness: 3,
+    neighborhood: "Marina"
+  },
+  {
+    location: "100 34th Ave.",
+    locationName: "Legend of Honor",  
+    type: "Museum",
+    cleanliness: 5,
+    neighborhood: "Presidio"
+  },
+  {
+    location: "735 7th Ave.",
+    locationName: "Safeway",  
+    type: "Grocery Store",
+    cleanliness: 2,
+    neighborhood: "Richmond"
+  },
+  {
+    location: "1396 La Playa St.",
+    locationName: "Java Beach Cafe",  
+    type: "Cafe",
+    cleanliness: 3,
+    neighborhood: "Sunset"
+  },
+   {
+    location: "2001 37th Ave.",
+    locationName: "St. Ignatius College Prepatory",  
+    type: "School",
+    cleanliness: 4,
+    neighborhood: "Sunset"
+  },
+  {
+    location: "225 Bush St.",
     locationName: "General Assembly",
     type: "School",
     cleanliness: 5,
     neighborhood: "Financial District"
   },
   {
-    location: "100 Montgomery St",
+    location: "100 Montgomery St.",
     locationName: "Starbucks",
-    type: "Coffee shop",
+    type: "Cafe",
     cleanliness: 4,
-    neighborhood: "SOMA"
+    neighborhood: "Financial District"
   },
   {
-    location: "601 Mission St",
+    location: "601 Mission St.",
     locationName: "CVS",
     type: "Store",
     cleanliness: 3,
-    neighborhood: "Mission District"
+    neighborhood: "Financial District"
   },
   {
-    location: "598 Market St",
-    locationName: "Montgomery Station",  
-    type: "Subway station",
+    location: "598 Market St.",
+    locationName: "Montgomery St. BART Station",  
+    type: "Transit Station",
     cleanliness: 1,
     neighborhood: "Union Square"
-  }
+  },
+  {
+    location: "525 Market St.",
+    locationName: "Chipotle Mexican Grill",  
+    type: "Restaurant",
+    cleanliness: 4,
+    neighborhood: "Union Square"
+  },
+  {
+    location: "1701 Greenwich St.",
+    locationName: "Fort Mason Market & Deli",  
+    type: "Restaurant",
+    cleanliness: 5,
+    neighborhood: "Russian Hill"
+  } 
 ];
 
 var review_list = [
